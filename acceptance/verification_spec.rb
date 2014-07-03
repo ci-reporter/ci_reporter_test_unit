2c2d2c5ffbba13c03f580ca3d85babe2804c6e7e
Jake Goulding <jake.goulding@gmail.com>
2014-07-02 20:48:59 -0400

    Update acceptance tests to RSpec 3 style

--- acceptance/verification_spec.rb
+++ acceptance/verification_spec.rb
@@ -1,38 +1,55 @@
 require 'rexml/document'
+require 'ci/reporter/test_utils/accessor'
+require 'ci/reporter/test_utils/shared_examples'
+require 'rspec/collection_matchers'
 
 REPORTS_DIR = File.dirname(__FILE__) + '/reports'
 
 describe "Test::Unit acceptance" do
-  it "should generate two XML files" do
-    File.exist?(File.join(REPORTS_DIR, 'TEST-TestUnitExampleTestOne.xml')).should == true
-    File.exist?(File.join(REPORTS_DIR, 'TEST-TestUnitExampleTestTwo.xml')).should == true
+  include CI::Reporter::TestUtils::SharedExamples
+  Accessor = CI::Reporter::TestUtils::Accessor
+
+  let(:report_path1) { File.join(REPORTS_DIR, 'TEST-TestUnitExampleTestOne.xml') }
+  let(:report_path2) { File.join(REPORTS_DIR, 'TEST-TestUnitExampleTestTwo.xml') }
+
+  context "the first example" do
+    subject(:result) { Accessor.new(load_xml_result(report_path1)) }
+
+    it { is_expected.to have(1).errors }
+    it { is_expected.to have(1).failures }
+    it { is_expected.to have(1).testcases }
+
+    describe "the assertion count" do
+      subject { result.assertions_count }
+      it { is_expected.to eql 1 }
+    end
+
+    it "captures the STDOUT" do
+      expect(result.system_out).to eql "Some <![CDATA[on stdout]]>"
+    end
+
+    it_behaves_like "a report with consistent attribute counts"
   end
 
-  it "should have one error and one failure for TestUnitExampleTestOne" do
-    doc = File.open(File.join(REPORTS_DIR, 'TEST-TestUnitExampleTestOne.xml')) do |f|
-      REXML::Document.new(f)
+  context "the second example" do
+    subject(:result) { Accessor.new(load_xml_result(report_path2)) }
+
+    it { is_expected.to have(0).errors }
+    it { is_expected.to have(0).failures }
+    it { is_expected.to have(1).testcases }
+
+    describe "the assertion count" do
+      subject { result.assertions_count }
+      it { is_expected.to eql 1 }
     end
-    doc.root.attributes["errors"].should == "1"
-    doc.root.attributes["failures"].should == "1"
-    doc.root.attributes["assertions"].should == "1"
-    doc.root.attributes["tests"].should == "1"
-    doc.root.elements.to_a("/testsuite/testcase").size.should == 1
-    doc.root.elements.to_a("/testsuite/testcase/error").size.should == 1
-    doc.root.elements.to_a("/testsuite/testcase/failure").size.should == 1
-    doc.root.elements.to_a("/testsuite/system-out").first.texts.inject("") do |c,e|
-      c << e.value; c
-    end.strip.should == "Some <![CDATA[on stdout]]>"
+
+    it_behaves_like "a report with consistent attribute counts"
+    it_behaves_like "nothing was output"
   end
 
-  it "should have no errors or failures for TestUnitExampleTestTwo" do
-    doc = File.open(File.join(REPORTS_DIR, 'TEST-TestUnitExampleTestTwo.xml')) do |f|
+  def load_xml_result(path)
+    File.open(path) do |f|
       REXML::Document.new(f)
     end
-    doc.root.attributes["errors"].should == "0"
-    doc.root.attributes["failures"].should == "0"
-    doc.root.attributes["assertions"].should == "1"
-    doc.root.attributes["tests"].should == "1"
-    doc.root.elements.to_a("/testsuite/testcase").size.should == 1
-    doc.root.elements.to_a("/testsuite/testcase/failure").size.should == 0
   end
 end
